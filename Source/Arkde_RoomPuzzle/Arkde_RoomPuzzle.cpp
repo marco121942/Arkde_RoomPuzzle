1afd34442f5f774e13cd257145caf3cd0aa45e4c
marco121942 <sotomayormarcoantonio3@gmail.com>
2021-01-30 21:26:06 -0500

    Empezando el proyecto en Unreal

---
+++ Source/Arkde_RoomPuzzle/Arkde_RoomPuzzle.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Arkde_RoomPuzzle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkde_RoomPuzzle, "Arkde_RoomPuzzle" );
---
+++ Source/Arkde_RoomPuzzle/Arkde_RoomPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkde_RoomPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_ROOMPUZZLE_API AArkde_RoomPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
